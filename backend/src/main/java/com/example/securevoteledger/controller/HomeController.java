2c07ef5d9c79d94636d960c3365f2be499bb6e66
janhavi-2323 <chikhaliyajanhavi2006@gmail.com>
2026-06-11 15:02:29 +0530

    updated code

--- backend/src/main/java/com/example/securevoteledger/controller/HomeController.java
+++ backend/src/main/java/com/example/securevoteledger/controller/HomeController.java
@@ -1,5 +1,13 @@
 package com.example.securevoteledger.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
-    
+
+    @GetMapping("/")
+    public String home() {
+        return "Secure Vote Ledger Backend is running";
+    }
 }
