bd1782173c3f412124f28f74836d9aaf2fd18851
janhavi-2323 <chikhaliyajanhavi2006@gmail.com>
2026-06-11 15:08:58 +0530

    updated code

--- backend/src/main/java/com/example/securevoteledger/entity/VoteRecord.java
+++ backend/src/main/java/com/example/securevoteledger/entity/VoteRecord.java
@@ -10,33 +10,34 @@ public class VoteRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String username;
     private String constituency;
     private String candidate;
+    private String previousHash;
     private String voteHash; // blockchain reference
     private LocalDateTime timestamp;
-    private String previousHash;
 
     public VoteRecord() {}
 
-    public VoteRecord(String username,String constituency,String candidate,String previousHash,String voteHash) {
+    public VoteRecord(String username, String constituency, String candidate, String previousHash, String voteHash) {
+        
+        this.username = username;
+        this.constituency = constituency;
+        this.candidate = candidate;
+        this.previousHash = previousHash;
+        this.voteHash = voteHash;
+        this.timestamp = LocalDateTime.now();
+    }
 
-    this.username = username;
-    this.constituency = constituency;
-    this.candidate = candidate;
-    this.previousHash = previousHash;
-    this.voteHash = voteHash;
-    this.timestamp = LocalDateTime.now();
-}
 
     public Long getId() { return id; }
     public String getUsername() { return username; }
     public String getConstituency() { return constituency; }
     public String getCandidate() { return candidate; }
+    public String getPreviousHash() { return previousHash; }
     public String getVoteHash() { return voteHash; }
     public LocalDateTime getTimestamp() { return timestamp; }
-    public String getPreviousHash() {return previousHash;}
-
 
 }
