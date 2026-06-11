2f4dd4360a187b1db9767c6d535939964f33abca
janhavi-2323 <chikhaliyajanhavi2006@gmail.com>
2026-06-11 15:15:46 +0530

    updated code

--- backend/src/main/java/com/example/securevoteledger/service/UserService.java
+++ backend/src/main/java/com/example/securevoteledger/service/UserService.java
@@ -3,34 +3,32 @@ package com.example.securevoteledger.service;
 import com.example.securevoteledger.entity.User;
 import com.example.securevoteledger.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-   private final BCryptPasswordEncoder passwordEncoder;
-
-     public UserService(UserRepository userRepository,
-                   BCryptPasswordEncoder passwordEncoder) {
-    this.userRepository = userRepository;
-    this.passwordEncoder = passwordEncoder;
+    public UserService(UserRepository userRepository,
+                    BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public User register(String username, String password, String role) {
+    public User register(String username, String password, String role, String voterId, String constituency) {
 
         Optional<User> existingUser = userRepository.findByUsername(username);
         if (existingUser.isPresent()) {
             throw new RuntimeException("User already exists");
         }
-      
         String encodedPassword = passwordEncoder.encode(password);
         User user = new User(username, encodedPassword, role);
-
+        user.setVoterId(voterId);
+        user.setConstituency(constituency);
         return userRepository.save(user);
     }
 
@@ -41,10 +39,9 @@ public class UserService {
             return null;
         }
         User user = userOpt.get();
-       if (!passwordEncoder.matches(password, user.getPassword())) {
-        return null;
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            return null;
         }
-
         return user;
     }
 
@@ -64,8 +61,9 @@ public class UserService {
             userRepository.save(user);
         }
     }
+
     public long getTotalUsers() {
-    return userRepository.count();
-}
+        return userRepository.count();
+    }
 
 }
