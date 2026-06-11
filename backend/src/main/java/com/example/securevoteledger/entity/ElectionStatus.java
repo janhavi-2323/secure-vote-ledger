6eb07647cc30b20010738ce462f5b93f946f3bf8
janhavi-2323 <chikhaliyajanhavi2006@gmail.com>
2026-06-11 15:08:01 +0530

    updated code

--- backend/src/main/java/com/example/securevoteledger/entity/ElectionStatus.java
+++ backend/src/main/java/com/example/securevoteledger/entity/ElectionStatus.java
@@ -1,5 +1,21 @@
 package com.example.securevoteledger.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class ElectionStatus {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean open;
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
 }
